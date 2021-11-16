f328a476168794d68f5849f9b21a3a67ee0e2c00
Salman Shuaib <salman.shuaib@gmail.com>
2021-11-16 02:27:03 -0500

    translations are good

--- HeartGame.py
+++ HeartGame.py
@@ -31,42 +31,6 @@ Keyboard_App.geometry("380x660")
 #     # self.destroy()
 #     self.__init__()
 
-translate = random.choice(words)
-
-
-def select(value):
-    # Keyboard_App.destroy()
-    # if value == "Backspace":
-    # if entry.insert(0, value):
-    # entry.delete(0, "end")
-    # if value == keyboard.is_pressed('q'):
-    # print('You pressed Exit!')
-    if value == " Space ":
-        entry.insert(tkinter.END, '   ')
-
-    elif value == "Tab":
-        entry.insert(tkinter.END, '     ')
-    elif value == "/":
-        if entry.get("1.0", 'end-1c') == correctNOTATION:
-            # Sound.mute()
-            messagebox.showinfo("Result", "CORRECT!")
-            translate = random.choice(words)
-            label1 = Label(Keyboard_App, text="Visual Memory TABLET"+"\n"+translate, font=(
-                "arial", 20, 'bold'), fg='yellow', bg='blue').grid(row=0, columnspan=40, padx=30, sticky=W)
-        elif entry.get("1.0", 'end-1c') == "e":
-            messagebox.showinfo("Bye", "You pressed EXIT!")
-            Keyboard_App.destroy()
-        else:
-            # Sound.mute()
-            messagebox.showinfo(
-                "Result", "Incorrect; the Correct answer is:\n                   " + correctNOTATION)
-            translate = random.choice(words)
-            label1 = Label(Keyboard_App, text="Visual Memory TABLET"+"\n"+translate, font=(
-                "arial", 20, 'bold'), fg='yellow', bg='blue').grid(row=0, columnspan=40, padx=30, sticky=W)
-            # refresh(Keyboard_App)
-    else:
-        entry.insert(tkinter.END, value)
-
 
 # def cycle_words(seed):
 
@@ -89,102 +53,157 @@ buttons = ['i', 'r', '|', '9',
 varRow = 2
 varColumn = 0
 
-correctNotationInitial = translate[0]
+
+def interpreter(SourceWord):
+    correctNotationInitial = SourceWord[0]
+
+    # Alphabet Numerical Equivalence follows:
+    if correctNotationInitial == '0':
+        Box = "00"
+    elif correctNotationInitial == 'a':
+        Box = "01"
+    elif correctNotationInitial == 'b':
+        Box = "02"
+    elif correctNotationInitial == 'c':
+        Box = "03"
+    elif correctNotationInitial == 'd':
+        Box = "04"
+    elif correctNotationInitial == 'e':
+        Box = "05"
+    elif correctNotationInitial == 'f':
+        Box = "06"
+    elif correctNotationInitial == 'g':
+        Box = "07"
+    elif correctNotationInitial == 'h':
+        Box = "08"
+    elif correctNotationInitial == 'i':
+        Box = "09"
+    elif correctNotationInitial == '1':
+        Box = "10"
+    elif correctNotationInitial == 'j':
+        Box = "11"
+    elif correctNotationInitial == 'k':
+        Box = "12"
+    elif correctNotationInitial == 'l':
+        Box = "13"
+    elif correctNotationInitial == 'm':
+        Box = "14"
+    elif correctNotationInitial == 'n':
+        Box = "15"
+    elif correctNotationInitial == 'o':
+        Box = "16"
+    elif correctNotationInitial == 'p':
+        Box = "17"
+    elif correctNotationInitial == 'q':
+        Box = "18"
+    elif correctNotationInitial == 'r':
+        Box = "19"
+    elif correctNotationInitial == '2':
+        Box = "20"
+    elif correctNotationInitial == 's':
+        Box = "21"
+    elif correctNotationInitial == 't':
+        Box = "22"
+    elif correctNotationInitial == 'u':
+        Box = "23"
+    elif correctNotationInitial == 'v':
+        Box = "24"
+    elif correctNotationInitial == 'w':
+        Box = "25"
+    elif correctNotationInitial == 'x':
+        Box = "26"
+    elif correctNotationInitial == 'y':
+        Box = "27"
+    elif correctNotationInitial == 'z':
+        Box = "28"
+
+    BoxFirst = Box[0]
+    BoxSecond = Box[1]
+    length = str(len(SourceWord))
+
+    # Root letters
+    if BoxSecond == "1":
+        ANE = "a"
+    elif BoxSecond == "2":
+        ANE = "b"
+    elif BoxSecond == "3":
+        ANE = "c"
+    elif BoxSecond == "4":
+        ANE = "d"
+    elif BoxSecond == "5":
+        ANE = "e"
+    elif BoxSecond == "6":
+        ANE = "f"
+    elif BoxSecond == "7":
+        ANE = "g"
+    elif BoxSecond == "8":
+        ANE = "h"
+    elif BoxSecond == "9":
+        ANE = "i"
+
+    correctNOTATION = BoxFirst+BoxSecond+ANE+length
+
+    # SourceWord = random.choice(words)
+    # # correctNOTATION = interpreter(translate)
+    label1 = Label(Keyboard_App, text="Visual Memory TABLET"+"\n"+SourceWord, font=(
+        "arial", 20, 'bold'), fg='yellow', bg='blue').grid(row=0, columnspan=40, padx=30, sticky=W)
+
+    print(correctNOTATION + " " + SourceWord)
+    return correctNOTATION
 
 
-# Alphabet Numerical Equivalence follows:
-if correctNotationInitial == '0':
-    Box = "00"
-elif correctNotationInitial == 'a':
-    Box = "01"
-elif correctNotationInitial == 'b':
-    Box = "02"
-elif correctNotationInitial == 'c':
-    Box = "03"
-elif correctNotationInitial == 'd':
-    Box = "04"
-elif correctNotationInitial == 'e':
-    Box = "05"
-elif correctNotationInitial == 'f':
-    Box = "06"
-elif correctNotationInitial == 'g':
-    Box = "07"
-elif correctNotationInitial == 'h':
-    Box = "08"
-elif correctNotationInitial == 'i':
-    Box = "09"
-elif correctNotationInitial == '1':
-    Box = "10"
-elif correctNotationInitial == 'j':
-    Box = "11"
-elif correctNotationInitial == 'k':
-    Box = "12"
-elif correctNotationInitial == 'l':
-    Box = "13"
-elif correctNotationInitial == 'm':
-    Box = "14"
-elif correctNotationInitial == 'n':
-    Box = "15"
-elif correctNotationInitial == 'o':
-    Box = "16"
-elif correctNotationInitial == 'p':
-    Box = "17"
-elif correctNotationInitial == 'q':
-    Box = "18"
-elif correctNotationInitial == 'r':
-    Box = "19"
-elif correctNotationInitial == '2':
-    Box = "20"
-elif correctNotationInitial == 's':
-    Box = "21"
-elif correctNotationInitial == 't':
-    Box = "22"
-elif correctNotationInitial == 'u':
-    Box = "23"
-elif correctNotationInitial == 'v':
-    Box = "24"
-elif correctNotationInitial == 'w':
-    Box = "25"
-elif correctNotationInitial == 'x':
-    Box = "26"
-elif correctNotationInitial == 'y':
-    Box = "27"
-elif correctNotationInitial == 'z':
-    Box = "28"
-
-BoxFirst = Box[0]
-BoxSecond = Box[1]
-length = str(len(translate))
-
-# Root letters
-if BoxSecond == "1":
-    ANE = "a"
-elif BoxSecond == "2":
-    ANE = "b"
-elif BoxSecond == "3":
-    ANE = "c"
-elif BoxSecond == "4":
-    ANE = "d"
-elif BoxSecond == "5":
-    ANE = "e"
-elif BoxSecond == "6":
-    ANE = "f"
-elif BoxSecond == "7":
-    ANE = "g"
-elif BoxSecond == "8":
-    ANE = "h"
-elif BoxSecond == "9":
-    ANE = "i"
-
-correctNOTATION = BoxFirst+BoxSecond+ANE+length
-
-print(correctNOTATION)
+# translate = random.choice(words)
+# # correctNOTATION = interpreter(translate)
+# label1 = Label(Keyboard_App, text="Visual Memory TABLET"+"\n"+translate, font=(
+#     "arial", 20, 'bold'), fg='yellow', bg='blue').grid(row=0, columnspan=40, padx=30, sticky=W)
+# interpreter(translate)
 
 
-def get_value():
-    e_text = entry.get()
-    if e_text == correctNOTATION:
-        print("Correct")
+def select(value):
+    translate = random.choice(words)
+    correctNOTATION = interpreter(translate)
+    # Keyboard_App.destroy()
+    # if value == "Backspace":
+    # if entry.insert(0, value):
+    # entry.delete(0, "end")
+    # if value == keyboard.is_pressed('q'):
+    # print('You pressed Exit!')
+
+    if value == " Space ":
+        entry.insert(tkinter.END, '   ')
+
+    elif value == "Tab":
+        entry.insert(tkinter.END, '     ')
+    elif value == "/":
+        # translate = random.choice(words)
+        # correctNOTATION = interpreter(translate)
+        if entry.get("1.0", 'end-1c') == correctNOTATION:
+            # Sound.mute()
+            messagebox.showinfo("Result", "CORRECT!")
+            # interpreter(translate)
+            label1 = Label(Keyboard_App, text="Visual Memory TABLET"+"\n"+translate, font=(
+                "arial", 20, 'bold'), fg='yellow', bg='blue').grid(row=0, columnspan=40, padx=30, sticky=W)
+        elif entry.get("1.0", 'end-1c') == "e":
+            messagebox.showinfo("Bye", "You pressed EXIT!")
+            Keyboard_App.destroy()
+        else:
+            # Sound.mute()
+            messagebox.showinfo(
+                "Result", "Incorrect; the Correct answer is:\n                   " + correctNOTATION)
+            # interpreter(translate)
+            # translate = random.choice(words)
+            # correctNOTATION = interpreter(translate)
+            label1 = Label(Keyboard_App, text="Visual Memory TABLET"+"\n"+translate, font=(
+                "arial", 20, 'bold'), fg='yellow', bg='blue').grid(row=0, columnspan=40, padx=30, sticky=W)
+            # refresh(Keyboard_App)
+    else:
+        entry.insert(tkinter.END, value)
+
+
+# def get_value():
+#     e_text = entry.get()
+#     if e_text == interpreter(translate):
+#         print("Correct")
 
 
 # if entry == correctNOTATION:
