d8ef297d45fbbc5a290356a7a3dd2f648e9e9613
Salman Shuaib <salman.shuaib@gmail.com>
2021-12-28 21:58:15 -0500

    working version, saved

--- HeartGame.py
+++ HeartGame.py
@@ -2,10 +2,14 @@
 
 # "How to Create a GUI Onscreen Keyboard in Python - Tutorial"
 
+# Reference format: "lead" = 12c4
+
 from tkinter import*
 window = Tk()
+window.geometry('500x400')
+inpu = Entry(window, width=100, bg='blue')
 
-"lead" = 12c+4
+button = ['a', 'b', 'c', 'd']
 
-
+inpu.grid(row=0, column=0)
 window.mainloop()
