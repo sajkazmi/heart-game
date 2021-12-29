de559bc158ba0f78ee6f9579604b9466945bf18d
Salman Shuaib <salman.shuaib@gmail.com>
2021-12-28 22:45:06 -0500

    buttons

--- HeartGame.py
+++ HeartGame.py
@@ -9,7 +9,16 @@ window = Tk()
 window.geometry('500x400')
 inpu = Entry(window, width=100, bg='blue')
 
-button = ['a', 'b', 'c', 'd']
+buttons = ['i', 'r', '|', '9',
+           'h', 'q', 'z', '8',
+           'g', 'p', 'y', '7',
+           'f', 'o', 'x', '6',
+           'e', 'n', 'w', '5',
+           'd', 'm', 'v', '4',
+           'c', 'l', 'u', '3',
+           'b', 'k', 't', '2',
+           'a', 'j', 's', '1',
+           '0', '1', '2', '/']
 
 inpu.grid(row=0, column=0)
 window.mainloop()
